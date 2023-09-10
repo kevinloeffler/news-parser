e47291bb11b63ab1d13eeb2d63241c56bcbd7580
kevinloeffler <hi@kevinloeffler.ch>
2023-09-10 12:40:07 +0200

    add sort and merge methods

--- util.py
+++ util.py
@@ -29,10 +29,56 @@ def create_unique_filename(filename: str, suffix: str) -> str:
 
 
 def create_new_parser_result_file(name: str) -> str:
-    filename = create_unique_filename(f'results/{name}'.replace(" ", "_"), '.csv')
+    filename = create_unique_filename(f'parser-results/{name}'.replace(" ", "_"), '.csv')
     with open(filename, 'x', newline='') as file:
         writer = csv.writer(file)
         header_row = ['date', 'klimastreik', 'klima', 'streik']
         writer.writerow(header_row)
 
     return filename
+
+
+def sort_and_merge_parser_result(path_to_parser_result: str, path_to_output_file: str):
+    with open(path_to_output_file, 'x') as output_file:
+        writer = csv.writer(output_file)
+
+        with open(path_to_parser_result, 'r') as input_file:
+            reader = csv.reader(input_file)
+
+            header_row = next(reader)
+            writer.writerow(header_row)
+
+            parser_result = []
+            for row in reader:
+                parser_result.append({
+                    'date': row[0],
+                    'match': {'klimastreik': int(row[1]), 'klima': int(row[2]), 'streik': int(row[3])}
+                })
+
+            sorted_parser_result = sort_parser_result(parser_result)
+            merged_parser_result = merge_parser_result(sorted_parser_result)
+
+            for result in merged_parser_result:
+                writer.writerow([
+                    result['date'],
+                    result['match']['klimastreik'],
+                    result['match']['klima'],
+                    result['match']['streik']
+                ])
+
+
+def sort_parser_result(parser_result: list[dict[str, any, dict[str, int]]]) -> list[dict[str, any, dict[str, int]]]:
+    return sorted(parser_result, key=lambda row: row['date'])
+
+
+def merge_parser_result(sorted_parser_result: list[dict[str, any, dict[str, int]]]) -> list[dict[str, any, dict[str, int]]]:
+    merged_list = [sorted_parser_result[0]]
+
+    for row in sorted_parser_result[1:]:
+        if merged_list[-1]['date'] == row['date']:
+            for key, value in merged_list[-1]['match'].items():
+                merged_list[-1]['match'][key] += row['match'][key]
+        else:
+            merged_list.append(row)
+
+    return merged_list
